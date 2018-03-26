67d5e9c77b979626a3d8a01f88091f2092209740
songyingchun <1033641437@qq.com>
2018-03-26 14:43:22 +0800

    路由切换

---
+++ src/components/activePublic.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="activePublic ">
+    <!-- 步骤组件 -->
+    <el-steps :space="200" default-active="/step1" class="step" :route="true">
+      <el-step title="活动信息" index="/step1" description=""></el-step>
+      <el-step title="报名签到" description=""></el-step>
+      <el-step title="分享设置" description=""></el-step>
+      <el-step title="个性设置" description=""></el-step>
+    </el-steps>
+    <!-- 视图 -->
+    <router-view class="view"></router-view>
+    <div class="but-group">
+      <el-button>预览</el-button>
+      <el-button>上一步</el-button>
+      <el-button type="primary">下一步</el-button>
+      <el-button type="primary">发布活动</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "active-public"
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import ActivePublic from '@/components/activePublic'
+import Step1 from '@/components/step1'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/activePublic',
+      name: 'activePublic',
+      component: ActivePublic,
+      children: [
+        {
+          path: 'step1',
+          name: 'step1',
+          component: Step1
+        }
+      ]
     }
   ]
 })
