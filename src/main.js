2f2cbf9fc1270e3dd77035cb2af5c6b43459c7cc
songyingchun <1033641437@qq.com>
2018-03-26 10:54:21 +0800

    '添加vue-resource和vuex'

--- src/main.js
+++ src/main.js
@@ -5,9 +5,14 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
+import VueResource from 'vue-resource'
+import Vuex from 'vuex'
+
 
 Vue.config.productionTip = false;
 
+Vue.use(Vuex)
+Vue.use(VueResource);
 Vue.use(ElementUI);
 /* eslint-disable no-new */
 new Vue({
